7964e7f47fae807f9f8a0237b62c69debff55497
bunnymonster <webert2575@my.uwstout.edu>
2018-04-06 20:53:26 -0500

    Add: added problem 14 to projectEuler.

--- java/ProjectEuler/ProjectEulerOneToTen/src/projectproblems/Problem11To15.java
+++ java/ProjectEuler/ProjectEulerOneToTen/src/projectproblems/Problem11To15.java
@@ -114,6 +114,23 @@ public class Problem11To15 {
         System.out.println(triangleNumber);
     }
     
+    
+    public void computeProblem14() {
+        
+        //For 1 to 1000000 compute collatz sequence length.
+        //If sequence length of integer k is greater than max,
+        //set max to the sequence length of k.
+        int maxLength = 1;
+        int collatzLength = 1;
+        for(int i = 1; i <= 1000000; i++) {
+            collatzLength = collatzSequence(i);
+            if(maxLength < collatzLength) {
+                maxLength = collatzLength;
+            }
+        }
+        System.out.println(maxLength);
+    }
+    
     //Problem 11
     //Compute maxProduct for four consecutive numbers in array.
     public long findMaxProduct(int[][] array, int firstIndex, int secondIndex, int firstIndexDir, int secondIndexDir, long maxProduct) {
@@ -146,5 +163,24 @@ public class Problem11To15 {
             count--;
         }
         return count;
+    }
+    
+    //Problem 14. compute collatz sequence length for given n.
+    //Collatz sequence defined as follows:
+    //n -> n/2 if n even
+    //n -> 3*n+1 if n odd.
+    public int collatzSequence(int n) {
+    
+        int sequenceLen = 1;
+        long value = n;
+        while(value != 1) {
+            if(value % 2 == 0) {
+                value = value/2;
+            } else {
+                value = 3*value + 1;
+            }
+            sequenceLen++;
+        }
+        return sequenceLen;
     }
 }
